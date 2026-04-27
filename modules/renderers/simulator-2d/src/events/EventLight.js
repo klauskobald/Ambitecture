18cbfa057ed0dc5e7a1cf0239ce5ed82d636370b
Klaus Kobald <klaus@kobald.com>
2026-04-27 12:16:45 -0500

    feat(simulator-2d): add EventLight class (square + label)

---
+++ modules/renderers/simulator-2d/src/events/EventLight.js
@@ -0,0 +1,22 @@
+class EventLight extends EventBase {
+    constructor(event, drawConfig) {
+        super(event, drawConfig);
+        const colorData = event.params?.color;
+        if (colorData) {
+            const { r, g, b } = Color.fromXYY(colorData).toRGB();
+            this._fillColor = `rgb(${Math.round(r * 255)},${Math.round(g * 255)},${Math.round(b * 255)})`;
+        } else {
+            this._fillColor = '#333';
+        }
+    }
+
+    draw(ctx, cx, cy, ppm) {
+        const half = (this._drawConfig.square.size * ppm) / 2;
+        ctx.fillStyle = this._fillColor;
+        ctx.fillRect(cx - half, cy - half, half * 2, half * 2);
+        ctx.strokeStyle = '#888';
+        ctx.lineWidth = 1;
+        ctx.strokeRect(cx - half, cy - half, half * 2, half * 2);
+        CanvasDraw.drawLabel(ctx, cx, cy, half, `L${this._layer}`);
+    }
+}

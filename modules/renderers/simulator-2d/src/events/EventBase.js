e644125ab73c70cd8feb1d14ff13eb5387c0950c
Klaus Kobald <klaus@kobald.com>
2026-04-27 12:10:58 -0500

    feat(simulator-2d): add EventBase abstract class

---
+++ modules/renderers/simulator-2d/src/events/EventBase.js
@@ -0,0 +1,11 @@
+class EventBase {
+    constructor(event, drawConfig) {
+        this._layer    = event.params?.layer ?? 0;
+        this._position = event.position ?? [0, 0, 0];
+        this._drawConfig = drawConfig;
+    }
+
+    draw(_ctx, _cx, _cy, _ppm) {
+        throw new Error(`${this.constructor.name} must implement draw()`);
+    }
+}

9a5934b068a48d1fad186970d553f188bbb2e48c
Klaus Kobald <klaus@kobald.com>
2026-05-31 21:11:45 +0200

    remove depricated

--- modules/controllers/surface-v1/src/edit/actionEdit/sceneActionParams.js
+++
@@ -1,7 +0,0 @@
-/**
- * Scene-target actions have no param editor today.
- * @param {HTMLElement} paramHost
- */
-export function renderSceneActionParams (paramHost) {
-  paramHost.replaceChildren()
-}
--- modules/controllers/surface-v1/src/edit/performResetKeyMetas.js
+++
@@ -1,3 +0,0 @@
-export const PERFORM_RESET_KEY_METAS = [
-  { key: 'scene', title: 'Scene switch' }
-]
